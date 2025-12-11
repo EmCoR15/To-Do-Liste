dad5860d97c601883a6150608839ac122c64fc1f
Vladimir Pruzhanskyi <livs1h@hotmail.de>
2025-12-11 22:12:06 +0100

    Datenbankanbindung 1.0

--- src/main/java/webtech/toDoListe/ToDoEntryController.java
+++ src/main/java/webtech/toDoListe/ToDoEntryController.java
@@ -1,25 +1,39 @@
 package webtech.toDoListe;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import java.time.LocalDateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class ToDoEntryController {
 
+    @Autowired
+    ToDoEntryService service;
+
+    Logger logger = LoggerFactory.getLogger(ToDoEntryController.class);
+
+    @CrossOrigin
+    @PostMapping("/todos")
+    public ToDoEntry createTodo(@RequestBody ToDoEntry todo) {
+        logger.info("POST /todos mit Name={}", todo.getName());
+        return service.save(todo);
+    }
+
+    @CrossOrigin
+    @GetMapping("/todos/{id}")
+    public ToDoEntry getTodo(@PathVariable String id) {
+        logger.info("GET /todos/{}", id);
+        Long todoId = Long.parseLong(id);
+        return service.get(todoId);
+    }
+
+    @CrossOrigin
     @GetMapping("/todos")
-    public List<ToDoEntry> getTodoEntries() {
-        return List.of(
-                new ToDoEntry(
-                        "L1",
-                        "Beispielbeschreibung",
-                        LocalDateTime.now().plusDays(1),
-                        false
-                )
-        );
+    public List<ToDoEntry> getAllTodos() {
+        logger.info("GET /todos (alle)");
+        return service.getAll();
     }
 }
-
-
---
+++ src/main/java/webtech/toDoListe/ToDoEntryService.java
@@ -0,0 +1,33 @@
+package webtech.toDoListe;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ToDoEntryService {
+
+    @Autowired
+    ToDoEntryRepository repo;
+
+    public ToDoEntry save(ToDoEntry entry) {
+        // id lieber von der DB erzeugen lassen:
+        entry.setId(null);
+        return repo.save(entry);
+    }
+
+    public ToDoEntry get(Long id) {
+        return repo.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+    public List<ToDoEntry> getAll() {
+        Iterable<ToDoEntry> iterator = repo.findAll();
+        List<ToDoEntry> todos = new ArrayList<>();
+        for (ToDoEntry e : iterator) {
+            todos.add(e);
+        }
+        return todos;
+    }
+}
